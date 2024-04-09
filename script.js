83a0cf25a77d01adf84e444dc9152a379e154b6d
felipefkns <felipenascsilva8@gmail.com>
2024-04-09 07:38:04 -0300

    projeto inicial

--- script.js
+++ script.js
@@ -6,33 +6,37 @@ var email = document.getElementById("email");
 var telefone = document.getElementById("telefone");
 var cep = document.getElementById("cep");
 var logradouro = document.getElementById("logradouro");
-var nummero = document.getElementById("numero");
+var numero = document.getElementById("numero");
 var complemento = document.getElementById("complemento");
 var bairro = document.getElementById("bairro");
 var cidade = document.getElementById("cidade");
 var estado = document.getElementById("estado");
+ 
 function alertar(event){
     //alert("Você clicou no botão!!!!" + event);
-
-const url = `http://viacep.com.br/ws/${cep.value}/json`;
-fetch(url)
-.then(resposta=>resposta.json())
-.then(dados=>alert(dados.barro))
-
+ 
+    const url = `http://viacep.com.br/ws/${cep.value}/json`;
+    fetch(url)
+        .then(function (resposta){
+            return resposta.json()
+        })
+        .then(function (dados){
+            logradouro.value = dados.logradouro;
+            bairro.value = dados.bairro;
+            cidade.value = dados.localidade;
+            estado.value = dados.uf;
+        })
+ 
+}
+ 
+function saidaDeDados(){
     saida.innerText = "Nome: " + nome.value +
-    "\n Email: " + email.value +
-    "\n Telefone: " + telefone.value +
-    "\n CEP: " + cep.value +
-    "\n Logradouro: " + logradouro.value +
-    "\n Numero: " + numero.value +
-    "\n Complemeto: " + complemento.value +
-    "\n Bairro: " + bairro.value +
-    "\n Cidade: " + cidade.value +
-    "\n Estado: " + estado.value ;
-
-
-
-
-
-
+    "\n Email:  " + email.value +
+    "\n telefone:  " + telefone.value +
+    "\n logradouro: " + logradouro.value +
+    "\n número: " + número.value +
+    "\n complemento: " + complemento.value +
+    "\n bairro: " + bairro.value +
+    "\n cidade: " + cidade.value +
+    "\n estado: " + estado.value;
 }
